eea0ae16c3c4f6d05f3c08ab94befd2b29e849af
ivg2 <ivg2@illinois.edu>
2018-09-15 16:44:47 -0500

    Playing around with components and props for part one.

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
+  constructor(props) {
+    super(props)
+    this.state = this.handleDisplayImage.bind(this)
+  }
+
+  handleDisplayImage() {
+    this.setState({ shouldDisplayImage: true })
+  }
+
   render() {
     return (
       <div className="instructions">
-        Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
+        if (this.state.shouldDisplayImage){' '}
+        {
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        }
       </div>
     )
   }
