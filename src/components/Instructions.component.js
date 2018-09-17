3af63f28276575c0507bc86d8f572182caa40f93
ivg2 <ivg2@illinois.edu>
2018-09-17 18:26:50 -0500

    Finished first two sections of react.

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -9,14 +9,25 @@ class Instructions extends Component {
   render() {
     return (
       <div className="instructions">
-        if ({this.props.shouldDisplayImage})
-        {
+        {this.props.shouldDisplayImage && (
           <img
             className="instructions__logo"
             src="https://uiuc.hack4impact.org/img/colored-logo.png"
             alt="h4i logo"
           />
-        }
+        )}
+
+        {this.props.items.map((item, index) => (
+          <ul key={index}>
+            {item.length >= 3 && (
+              <li>
+                {this.props.items.indexOf(item) % 2 === 1
+                  ? item.toUpperCase()
+                  : item}
+              </li>
+            )}
+          </ul>
+        ))}
       </div>
     )
   }
